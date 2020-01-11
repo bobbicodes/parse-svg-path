a050e2812ab6a3611e3621c2a398754f405df00f
porkostomus <porkostomus@mail.com>
2020-01-11 06:43:22 -0800

    compute dimensions of SVG path

--- public/js/cljs-runtime/parse_svg_path.main.js
+++ public/js/cljs-runtime/parse_svg_path.main.js
@@ -9,17 +9,80 @@ parse_svg_path.main.parse_commands = (function parse_svg_path$main$parse_command
 return clojure.string.split.cljs$core$IFn$_invoke$arity$2(s,/(?=[A-Z])/);
 });
 /**
- * Parses the command's arguments into a sequence of integers
+ * Parses the command's parameters into a sequence of numbers
  */
 parse_svg_path.main.parse_vals = (function parse_svg_path$main$parse_vals(s){
-return cljs.core.map.cljs$core$IFn$_invoke$arity$2(parseInt,cljs.core.re_seq(/[\d.]+/,cljs.core.subs.cljs$core$IFn$_invoke$arity$2(s,(1))));
+return cljs.core.map.cljs$core$IFn$_invoke$arity$2(parseFloat,cljs.core.re_seq(/[\d.]+/,cljs.core.subs.cljs$core$IFn$_invoke$arity$2(s,(1))));
 });
 /**
  * Takes an SVG path string and outputs a seq of vectors
- * containing each command paired with its args
+ * containing each command paired with its params
  */
 parse_svg_path.main.parse_svg_path = (function parse_svg_path$main$parse_svg_path(s){
 return cljs.core.map.cljs$core$IFn$_invoke$arity$2(cljs.core.juxt.cljs$core$IFn$_invoke$arity$2(cljs.core.first,parse_svg_path.main.parse_vals),parse_svg_path.main.parse_commands(s));
+});
+parse_svg_path.main.all_vals = (function parse_svg_path$main$all_vals(s){
+return cljs.core.map.cljs$core$IFn$_invoke$arity$2(parseFloat,cljs.core.re_seq(/\d+\.?\d*|\d*\.?\d+/,s));
+});
+parse_svg_path.main.x_val = (function parse_svg_path$main$x_val(p__31916){
+var vec__31917 = p__31916;
+var command = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__31917,(0),null);
+var params = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__31917,(1),null);
+var G__31920 = command;
+switch (G__31920) {
+case "C":
+case "S":
+case "Q":
+return cljs.core.nth.cljs$core$IFn$_invoke$arity$2(params,(2));
+
+break;
+case "A":
+return cljs.core.nth.cljs$core$IFn$_invoke$arity$2(params,(5));
+
+break;
+default:
+return cljs.core.first(params);
+
+}
+});
+parse_svg_path.main.y_val = (function parse_svg_path$main$y_val(p__31921){
+var vec__31922 = p__31921;
+var command = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__31922,(0),null);
+var params = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__31922,(1),null);
+var G__31925 = command;
+switch (G__31925) {
+case "C":
+case "S":
+case "Q":
+return cljs.core.nth.cljs$core$IFn$_invoke$arity$2(params,(3));
+
+break;
+case "A":
+return cljs.core.nth.cljs$core$IFn$_invoke$arity$2(params,(6));
+
+break;
+default:
+return cljs.core.second(params);
+
+}
+});
+parse_svg_path.main.x_min = (function parse_svg_path$main$x_min(s){
+return cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.min,cljs.core.remove.cljs$core$IFn$_invoke$arity$2(cljs.core.nil_QMARK_,cljs.core.map.cljs$core$IFn$_invoke$arity$2(parse_svg_path.main.x_val,parse_svg_path.main.parse_svg_path(s))));
+});
+parse_svg_path.main.y_min = (function parse_svg_path$main$y_min(s){
+return cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.min,cljs.core.remove.cljs$core$IFn$_invoke$arity$2(cljs.core.nil_QMARK_,cljs.core.map.cljs$core$IFn$_invoke$arity$2(parse_svg_path.main.y_val,parse_svg_path.main.parse_svg_path(s))));
+});
+parse_svg_path.main.x_max = (function parse_svg_path$main$x_max(s){
+return cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.max,cljs.core.map.cljs$core$IFn$_invoke$arity$2(parse_svg_path.main.x_val,parse_svg_path.main.parse_svg_path(s)));
+});
+parse_svg_path.main.y_max = (function parse_svg_path$main$y_max(s){
+return cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.max,cljs.core.map.cljs$core$IFn$_invoke$arity$2(parse_svg_path.main.y_val,parse_svg_path.main.parse_svg_path(s)));
+});
+parse_svg_path.main.width = (function parse_svg_path$main$width(s){
+return (parse_svg_path.main.x_max(s) - parse_svg_path.main.x_min(s));
+});
+parse_svg_path.main.height = (function parse_svg_path$main$height(s){
+return (parse_svg_path.main.y_max(s) - parse_svg_path.main.y_min(s));
 });
 parse_svg_path.main.app = (function parse_svg_path$main$app(){
 return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"margin","margin",-995903681),"auto",new cljs.core.Keyword(null,"margin-top","margin-top",392161226),"100px",new cljs.core.Keyword(null,"width","width",-384071477),"600px"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"parse-svg-path"], null)], null);
--- src/parse_svg_path/main.cljs
+++ src/parse_svg_path/main.cljs
@@ -9,18 +9,52 @@
   (str/split s #"(?=[A-Z])"))
 
 (defn parse-vals 
-  "Parses the command's arguments into a sequence of integers"
+  "Parses the command's parameters into a sequence of numbers"
   [s]
-  (map js/parseInt (re-seq #"[\d.]+" (subs s 1))))
+  (map js/parseFloat (re-seq #"[\d.]+" (subs s 1))))
 
 (defn parse-svg-path 
   "Takes an SVG path string and outputs a seq of vectors
-   containing each command paired with its args"
+   containing each command paired with its params"
   [s]
   (map (juxt first parse-vals) (parse-commands s)))
 
+(defn all-vals [s]
+  (map js/parseFloat (re-seq #"\d+\.?\d*|\d*\.?\d+" s)))
+
+(defn x-val [[command params]]
+  (case command
+    ("C" "S" "Q") (nth params 2)
+    "A" (nth params 5)
+    (first params)))
+
+(defn y-val [[command params]]
+  (case command
+    ("C" "S" "Q") (nth params 3)
+    "A" (nth params 6)
+    (second params)))
+
+(defn x-min [s]
+  (apply min (remove nil? (map x-val (parse-svg-path s)))))
+
+(defn y-min [s]
+  (apply min (remove nil? (map y-val (parse-svg-path s)))))
+
+(defn x-max [s]
+  (apply max (map x-val (parse-svg-path s))))
+
+(defn y-max [s]
+  (apply max (map y-val (parse-svg-path s))))
+
+(defn width [s]
+  (- (x-max s) (x-min s)))
+
+(defn height [s]
+  (- (y-max s) (y-min s)))
+
 (comment
-  (parse-svg-path "M132 523Q98 523 98 490V479L111 451Q186 293 220 178L228 154H196Q158 154 142 157T103 176Q92 183 81 194T62 215 53 227Q51 228 38 228 19 228 19 218 19 212 38 183T93 121 164 83Q171 81 389 81 549 81 553 82 573 89 573 110 573 141 541 152 535 154 472 154H408L405 171Q393 243 393 290 393 342 402 383T421 447 431 475Q431 492 417 507T381 522Q370 522 363 519T347 495 331 435Q330 426 330 391 330 342 339 286T357 194 367 154H269L268 158Q268 161 249 237T206 398 175 495Q164 523 132 523Z"))
+  (height "M132 523Q98 523 98 490V479L111 451Q186 293 220 178L228 154H196Q158 154 142 157T103 176Q92 183 81 194T62 215 53 227Q51 228 38 228 19 228 19 218 19 212 38 183T93 121 164 83Q171 81 389 81 549 81 553 82 573 89 573 110 573 141 541 152 535 154 472 154H408L405 171Q393 243 393 290 393 342 402 383T421 447 431 475Q431 492 417 507T381 522Q370 522 363 519T347 495 331 435Q330 426 330 391 330 342 339 286T357 194 367 154H269L268 158Q268 161 249 237T206 398 175 495Q164 523 132 523Z")
+  (x-max "M132 523Q98 523 98 490V479L111 451Q186 293 220 178L228 154H196Q158 154 142 157T103 176Q92 183 81 194T62 215 53 227Q51 228 38 228 19 228 19 218 19 212 38 183T93 121 164 83Q171 81 389 81 549 81 553 82 573 89 573 110 573 141 541 152 535 154 472 154H408L405 171Q393 243 393 290 393 342 402 383T421 447 431 475Q431 492 417 507T381 522Q370 522 363 519T347 495 331 435Q330 426 330 391 330 342 339 286T357 194 367 154H269L268 158Q268 161 249 237T206 398 175 495Q164 523 132 523Z"))
 
 (defn app []
   [:div {:style {:margin     "auto"
@@ -28,9 +62,9 @@
                  :width      "600px"}}
    [:h1 "parse-svg-path"]])
 
-(defn ^:dev/after-load start []
-  (r/render [app]
-            (.getElementById js/document "app")))
+            (defn ^:dev/after-load start []
+              (r/render [app]
+                        (.getElementById js/document "app")))
 
-(defn ^:export init []
-  (start))
+            (defn ^:export init []
+              (start))
